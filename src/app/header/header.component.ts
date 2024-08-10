98f304b11b8a7b46560a2831f4ca5411d46da893
santhoshkumar <its.santhoshkumar@outlook.com>
2024-08-10 20:08:39 +0530

    feat: styles

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,4 +6,6 @@ import { Component } from "@angular/core";
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+    data = true;
+}

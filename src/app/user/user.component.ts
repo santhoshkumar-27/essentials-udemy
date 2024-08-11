8809048747fd026e111349844f001d7bddb9fb02
santhoshkumar <its.santhoshkumar@outlook.com>
2024-08-11 11:16:00 +0530

    fix: refactor

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,5 +9,13 @@ import { DUMMY_USERS } from '../shared/Utility/dummy.data'
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[0]
+  selectedUser = DUMMY_USERS[0];
+
+  /**
+   * Resolving template complex handling 
+   * using in class
+   */
+  get imagePath() {
+    return `users/${this.selectedUser.avatar}`
+  }
 }

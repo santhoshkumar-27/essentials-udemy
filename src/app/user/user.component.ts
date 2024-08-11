a6c68cd9b9c1f298755d70c6fcf871f65eb8cc24
santhoshkumar <its.santhoshkumar@outlook.com>
2024-08-11 11:50:16 +0530

    fix: migrating normal state to the signal based state for updating

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 import { DUMMY_USERS } from '../shared/Utility/dummy.data'
 @Component({
@@ -9,19 +9,20 @@ import { DUMMY_USERS } from '../shared/Utility/dummy.data'
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[0];
-
+  selectedUser = signal(DUMMY_USERS[0]);
+  imagePath = computed(() => `users/${this.selectedUser().avatar}`);
   /**
    * Resolving template complex handling 
    * using in class
    */
-  get imagePath() {
-    return `users/${this.selectedUser.avatar}`
-  }
+  // get imagePath() {
+  //   return `users/${this.selectedUser().avatar}`
+  // }
 
   onSelectedUser() {
     console.log('this user is selected', this.selectedUser);
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    // this.selectedUser = DUMMY_USERS[randomIndex];
+    this.selectedUser.set(DUMMY_USERS[randomIndex])
   }
 }

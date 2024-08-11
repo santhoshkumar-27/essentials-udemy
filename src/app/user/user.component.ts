a8edfa0722d3754bec856c121f1de934819ccd2f
santhoshkumar <its.santhoshkumar@outlook.com>
2024-08-11 11:29:34 +0530

    fix: changing random users

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -18,4 +18,10 @@ export class UserComponent {
   get imagePath() {
     return `users/${this.selectedUser.avatar}`
   }
+
+  onSelectedUser() {
+    console.log('this user is selected', this.selectedUser);
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+    this.selectedUser = DUMMY_USERS[randomIndex];
+  }
 }

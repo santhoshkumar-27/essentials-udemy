470329164e7c8c09a99b2aa9922a69c8d3525ad2
santhoshkumar <its.santhoshkumar@outlook.com>
2024-08-17 11:29:54 +0530

    feat: added template users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { TaskComponent } from "./task/task.component";
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  users = DUMMY_USERS;
+  users: USER[] = DUMMY_USERS;
   selectedUserId!: string;
   title = 'essentials';
 

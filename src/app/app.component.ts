4f99429c141211dfc02d577aa399db6be735eed4
santhoshkumar <its.santhoshkumar@outlook.com>
2024-08-17 11:46:53 +0530

    feat: implemented tasks component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS, USER } from './shared/Utility/dummy.data';
-import { TaskComponent } from "./task/task.component";
+import { TasksComponent } from "./tasks/tasks.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TaskComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

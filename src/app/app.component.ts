144922c1b1a954749993ddc7a471dbe86ceb3bf8
santhoshkumar <its.santhoshkumar@outlook.com>
2024-08-17 11:11:54 +0530

    feat: added task component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,26 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
-import { DUMMY_USERS } from './shared/Utility/dummy.data';
+import { DUMMY_USERS, USER } from './shared/Utility/dummy.data';
+import { TaskComponent } from "./task/task.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId!: string;
   title = 'essentials';
 
+  get selectedUser() {
+    const user = this.users.find((user) => user.id === this.selectedUserId);
+    return user
+  }
+
   onSelectItem(id: string) {
-    console.log('this is selected item', id)
+    this.selectedUserId = id;
   }
 }
--- src/app/shared/Utility/dummy.data.ts
+++ src/app/shared/Utility/dummy.data.ts
@@ -30,3 +30,9 @@ export const DUMMY_USERS = [
         avatar: 'user-6.jpg',
     },
 ];
+
+export interface USER {
+    id: string;
+    name: string;
+    avatar: string;
+}
